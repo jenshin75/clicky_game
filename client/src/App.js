20a2f7c99b3156643b2430e31c0ae1de5d76b0f4
jenshin75 <49443598+jenshin75@users.noreply.github.com>
2019-09-05 18:28:44 -0500

    updated logic

--- client/src/App.js
+++ client/src/App.js
@@ -10,77 +10,58 @@ class App extends Component {
   state = {
     friends,
     score: 0,
-    topscore: 12,
-    clicked: []
+    topscore: 0
   };
 
   handleClick = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being clicked  
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    // this.setState({friends}); 
-    
+
+    // Filter this.state.friends for friends with an id not equal to the id being clicked
+    const cardClicked = this.state.friends.filter(friend => friend.id === id);
+    console.log(cardClicked)
+    console.log(cardClicked[0].clicked)
+
     // HERE: you need to look if the card was clicked before
-  if (friends[0].clicked) {
+    if (cardClicked[0].clicked === "true") {
       // if yes you need to init
-    friends[0].clicked = true;
-    this.handleIncrement();  
-  }    
-else 
-//    // if no you need to update scores and shuffle and then updete the state
-//   friends[0].clicked = true;
-this.handleDecrement(); 
-};
-
-    // (this.state.clicked.includes(id)){
-
-  // compare if the score is > topscore if ues update topsocre
-  // else{
-  // updte the array push the id to the clicked
-
-  // score++ (handleIncrement increments this.state.count by 1)
-  handleIncrement = () => {
-    // We always use the setState method to update a component's state
-    this.setState({ score: this.state.score + 1 });
-    this.shuffle(friends);
+      console.log("clicked before");
+      this.resetGame();
+    }
+    // if no you need to update scores and shuffle and then updete the state
+    else {
+      console.log("not clicked before");
+      const newFriends = this.state.friends.map(friend => {
+        if (friend.id === id) {
+          friend.clicked = "true";
+        }
+        return friend;
+      });
+      console.log(newFriends);
+      this.setState({
+        friends: newFriends.sort(() => Math.random() - 0.5),
+        score: this.state.score + 1,
+        topscore: this.state.score + 1 > this.state.topscore ? this.state.score + 1 : this.state.topscore
+      });
+    }
   };
 
-  handleDecrement = () => {
-    // We always use the setState method to update a component's state
-    this.setState({ score: this.state.score - 1 });
-    this.resetGame();
+  resetGame = () => {
+    const newFriends = this.state.friends.map(friend => {
+      friend.clicked = "false";
+      return friend;
+    });
+    console.log("reset", newFriends)
+    this.setState({
+      friends: newFriends.sort(() => Math.random() - 0.5),
+      score: 0
+    });
   };
-
-  // shuffle
-  shuffle = friends => {
-    // https://stackoverflow.com/a/43235780/10503606
-    let newFriends = friends.sort(() => Math.random() - 0.5);
-    return newFriends;
-  };
-
-
-  //}
-
-  // Set this.state.friends equal to the new friends array
-
-  //   this.setState({
-  //     friends: newFriends,
-  //     score: scoreTemp,
-  //     topscore: topscoreTemp,
-  //     clicked: clickedTemp
-  // });
-  // ;
 
   // Map over this.state.friends and render a FriendCard component for each friend object
-
   render() {
     return (
       <Wrapper>
         <Title>Click 2 Score</Title>
-        <Navbar1
-          score={this.state.score}
-          topscore={this.state.topscore}
-        />
+        <Navbar1 score={this.state.score} topscore={this.state.topscore} />
         {this.state.friends.map(friend => (
           <FriendCard
             handleClick={this.handleClick}
